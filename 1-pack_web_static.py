563b941abaa1b814bc794ab757388ed73f7a03dd
97Fangz <briansegem25@gmail.com>
2023-07-17 12:53:01 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" A Fabric script that generates a .tgz archive
-from the contents of the web_static folder in the AirBnB repository and lists all files"""
+""" A Fabric script that generates a .tgz archive from the contents of the web_static folder in the AirBnB repository """
+
 from datetime import datetime
 from fabric.api import local
 import os
 
 
 def do_pack():
-    """Creates a gzipped tar archive from the web_static/ content"""
+    """
+    Generates a gzipped tar archive from the contents of the web_static folder.
+
+    Returns:
+        Archive path if the archive has been correctly generated, otherwise None.
+    """
     try:
         if not os.path.exists("versions"):
             local('mkdir versions')
         now = datetime.now()
         ft = now.strftime("%Y%m%d%H%M%S")
         archive_path = "versions/web_static_{}.tgz".format(ft)
-        local("tar -cvzf {}  web_static/".format(archive_path))
+        local("tar -czvf {} web_static/".format(archive_path))
+
+        # Check if the archive has all the files from web_static
+        with local('tar -tf {} | grep web_static'.format(archive_path), capture=True) as output:
+            if len(output.stdout.strip()) == 0:
+                return None
+
         return archive_path
-    except Exception as e:
-        print("An exception occurred: {}".format(e))
+
+    except Exception:
         return None
